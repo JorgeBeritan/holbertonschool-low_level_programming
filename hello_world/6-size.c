ccf3bbb67955c73aca529c12567e7d5e28f5e1a1
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-15 12:07:46 -0300

    pls2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,15 +5,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{char a;
+{ char a;
 int b;
 long int c;
-long long int d;
+long long d;
 float e;
-printf("Size of a char: %ld bytes(s)\n", sizeof(a));
-printf("Size of an int: %ld bytes(s)\n", sizeof(b));
-printf("Size of a long int: %ld bytes(s)\n", sizeof(c));
-printf("Size of a long long int: %ld bytes(s)\n", sizeof(d));
-printf("Size of a float: %ld bytes(s)\n", sizeof(e));
-return (0);
-}
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of an int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of an float: %lu byte(s)\n", sizeof(e));
+return (0); }
