0ea61f318f1a7acb1e60478e39caf2dc1c155fc3
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-15 11:26:25 -0300

    pls2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,13 @@
 #include <stdio.h>
 int main(void)
 {
+
 char a;
 int b;
 long int c;
 long long int d;
 float e;
+
 printf("Size of a char: %ld byte(s)", sizeof(a));
 printf("Size of an int: %ld bytes(s)", sizeof(b));
 printf("Size of a long int: %ld bytes(s)", sizeof(c));
