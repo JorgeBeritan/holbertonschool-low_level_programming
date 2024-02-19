d6c49163cdb012ee1d3f8d47f6954c23cb570795
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-19 11:21:01 -0300

    Absolute Program

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
+/**
+  * _abs - entry point
+  * @c: value
+  *
+  * Return: always 0
+  */
 int _abs(int c)
 {
 	if (c < 0)
 	{
 		int abc = c * -1;
-		return (abc);		
-	}	
+
+		return (abc);
+	}
 	return (c);
 }
