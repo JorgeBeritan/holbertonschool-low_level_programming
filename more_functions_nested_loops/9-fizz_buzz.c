4f7aa7ef418354f72f1def2549ada7e8533c4bb0
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-20 07:27:01 -0300

    FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,13 +6,15 @@ int main(void)
 	for (n = 1; n < 101; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (n % 3 == 0)
 			printf("Fizz ");
 		else if (n % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", n);
+		if (n < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
