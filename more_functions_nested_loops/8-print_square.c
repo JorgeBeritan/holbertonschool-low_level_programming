5e323362458fb1f7bdff51f497b6459f8aac8d1b
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-20 05:58:37 -0300

    #print

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_square(int size)
+{
+	int j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (k = 0; k < size; k++)
+	{
+		if (size > 0)
+		{
+			for (j = 0; j < size; j++)
+				{
+					_putchar('#');
+				}
+		}
+			_putchar('\n');
+		}
+}
