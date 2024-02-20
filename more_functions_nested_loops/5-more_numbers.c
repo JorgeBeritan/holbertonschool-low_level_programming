c57062f30b5d26ce0b0fc9840466b1febefeb8be
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-20 03:25:11 -0300

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - entry point
+ * more_numbers - entry point
  *
  * Return: void
  */
 void more_numbers(void)
 {
 	int a, i;
+
 	i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		for (a = 0; a < 15; a++)
 		{
