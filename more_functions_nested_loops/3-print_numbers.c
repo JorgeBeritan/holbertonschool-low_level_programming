df9ccc62716b9270038df20eda18e2469455c691
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-20 02:42:43 -0300

    print number

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - entry point
+ *
+ * Return: void
+ */
+void print_number(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
