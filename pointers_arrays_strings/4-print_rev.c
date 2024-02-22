2bc66383fb8991bc843b8449e9364c747cf1f736
JorgeBeritan <inaikmcontacto.@gmail.com>
2024-02-22 12:51:49 -0300

    Rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,11 +5,12 @@ print_rev(char *s)
 {
 	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+
 	for (j = i - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
 	_putchar('\n');
-	
 }
