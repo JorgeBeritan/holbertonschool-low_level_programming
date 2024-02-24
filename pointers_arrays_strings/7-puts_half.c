4a37a3ba11ddb349b3b846c5513f7db5cc9a2d85
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-24 15:47:05 +0000

    Puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int k;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		k = (i -1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = i / 2;
+	}
+	for (; k < i; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}	
