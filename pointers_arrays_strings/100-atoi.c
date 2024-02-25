0b83840ec5f8abe07464ae304b0912dcda4a26dc
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-25 10:00:14 +0000

    Atoi Function

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int j = 0;
+	int sign = 1;
+	int i = 0;
+
+	while(s[i] == ' ')
+	{
+		i++;
+	}
+
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+	while (s[i] != '\0')
+	{
+		if(s[i] >= '0' && s[i] <= '9')
+		{
+			j = j * 10 + (s[i] - '0');
+		}
+		i++;
+	}
+
+	return (sign * j);
+}
