8f893d80bfb4a5f83cfe3b2cce4382425b0edc7b
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-26 10:56:38 -0300

    STRncpy

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,37 +2,26 @@
 
 int _atoi(char *s)
 {
-	int j = 0;
-	int sign = 1;
-	int i = 0;
+	int i = 0, j = 0, sign = 1;
 
-	while(s[i] == ' ')
+	while (s[i] == ' ')
 	{
 		i++;
 	}
 
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		sign = 1;
-		i++;
-	}
-	while (s[i] != '\0')
+	if (s[i] == '+' || s[i] == '-')
 	{
 		if (s[i] == '-')
 		{
-			sign *= -1;
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			j = j * 10 + (s[i] - '0');
+			sign = -1;
 		}
 		i++;
 	}
 
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+	{
+		j = j * 10 + (s[i] - '0');
+		i++;
+	}
 	return (sign * j);
 }
