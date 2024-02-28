d684168a83b8a1b45f6f9bda43d951c88225e0fa
JorgeBeritan <inaikmcontacto@gmail.com>
2024-02-28 00:21:08 +0000

    Capitalize

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	    
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            
+		if (str[i - 1] == 32 || str[i - 1] == 44 || str[i - 1] == 59 || str[i] == 46)
+		{
+			str[i] -=32;
+		}
+		if (str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40)
+		{
+			str[i + 1] -= 32;
+		}
+		if (str[i] == 41 || str[i] == 123 || str[i] == 125)
+		{
+			str[i + 1] -= str[i] - 32;
+		}
+		if (str[i] == 9 || str[i] == 10)
+		{
+			str[i + 1] -= 32;
+		}
+        }
+		i++;
+	}
+	return (str);
+}
