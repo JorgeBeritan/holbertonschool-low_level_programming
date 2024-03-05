a3e4d7a0ae5032699557d41fafb39b95cbe97706
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-05 18:55:54 +0000

    Concatenate

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -4,43 +4,32 @@
 char
 *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len = 0, tlen, i, j;
 	char *result;
+	unsigned int len = n, i;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	while (s1[len] != '\0')
-	{
+	for (i = 0; s1[i]; i++)
 		len++;
-	}
 
-	tlen = len + n;
-	result = malloc((tlen + 1) * sizeof(char));
+	result = malloc(sizeof(char) * (len + 1));
 
 	if (result == NULL)
-	{
 		return (NULL);
-	}
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		result[i] = s1[i];
-	}
+	len = 0;
 
-	for (j = 0; j < n && s2[j] != '\0'; j++)
-	{
-		result[i + j] = s2[j];
-	}
+	for (i = 0; s1[i]; i++)
+		result[len++] = s1[i];
 
-	result[i + n] = '\0';
+	for (i = 0; s2[i] && i < n; i++)
+		result[len++] = s2[i];
+
+	result[len] = '\0';
+
 	return (result);
-
 }
