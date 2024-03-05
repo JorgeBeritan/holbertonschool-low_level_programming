44a2fa0ada723e53b0b473656a525b3c456f7546
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-05 12:16:00 +0000

    alloc string

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,7 +3,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i;
+	int i, j;
 	int **grid;
 
 	if (width <= 0 || height <= 0)
@@ -20,7 +20,17 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
 			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
 		}
 	}
 	return (grid);
