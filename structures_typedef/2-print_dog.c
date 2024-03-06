cbfaf5e40a288254c1f308db978bbf4f6e5afdf5
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-06 14:20:49 +0000

    MY dog Printf

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("(nil)\n");
+		return;
+	}
+
+	printf("Name: ");
+	if ((*d).name == NULL)
+		printf("(nil)");
+	else
+		printf("%s", (*d).name);
+	printf("\n");
+
+	printf("Age: %f\n", (*d).age);
+
+	printf("Owner: ");
+	if ((*d).owner == NULL)
+		printf("(nil)");
+	else
+		printf("%s", (*d).owner);
+	printf("\n");
+
+}
