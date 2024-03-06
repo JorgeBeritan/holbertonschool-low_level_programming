2cc625ccaf2c9e0ca9db0ab5c4e05673fda7a294
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-06 13:52:11 +0000

    MY dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void
+init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
