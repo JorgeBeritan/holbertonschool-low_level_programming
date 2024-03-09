6177a9f959822262677851111239b5d0dcb38dd7
JorgeBeritan <inaikmcontact@gmail.com>
2024-03-09 12:30:54 +0000

    Program to calc

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+
+int
+(*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[0].f);
+
+		i++;
+	}
+	return (0);
+}
