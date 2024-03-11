770495d65a846680ccf5dbf6723cb3cf92759a6c
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-11 14:18:49 +0000

    searche a character

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
-char *_strchr(char *s, char c)
+char
+*_strchr(char *s, char c)
 {
 	for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 			return ((char *)s);
 	}
+	if (*s == c)
+		return (s);
 	return (NULL);
 }
