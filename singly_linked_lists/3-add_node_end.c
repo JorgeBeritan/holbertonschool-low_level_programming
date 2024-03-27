c13fcba7d094672bf535ff63ed62b8c3bb71f3a8
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-27 09:34:07 -0400

    Hot fix 130 add

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -5,34 +5,36 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *pointer;
 	unsigned int len;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{ 
-		return (NULL);
-	}
-
-	while (str[len])
+	if (str != NULL)
 	{
-		len++;
-	}
-
-	new->len = len;
-	new->str = strdup(str);
-	if (*head == NULL)
-	{
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		new->next = NULL;
-		pointer = *head;
-		while (pointer->next)
-		{
-			pointer = pointer->next;
+		new = malloc(sizeof(list_t));
+		if (new == NULL)
+		{ 
+			return (NULL);
 		}
-		pointer->next = new;
-	}
 
+		while (str[len])
+		{
+			len++;
+		}
+
+		new->len = len;
+		new->str = strdup(str);
+		if (*head == NULL)
+		{
+			new->next = *head;
+			*head = new;
+		}
+		else
+		{
+			new->next = NULL;
+			pointer = *head;
+			while (pointer->next)
+			{
+				pointer = pointer->next;
+			}
+			pointer->next = new;
+		}
+	}
 	return (new);
 }
