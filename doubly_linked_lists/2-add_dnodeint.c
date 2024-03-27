8592c4ef19d4006f1c892999fa8ec5b10dd8ca69
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-27 13:32:14 -0300

    New node on DLL

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+dlistint_t
+*add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	if (*head)
+	{
+		newnode->next = *head;
+		newnode->prev = (*head)->prev;
+		(*head)->prev = newnode;
+		*head = newnode;
+		return (*head);
+	}
+
+	newnode->next = *head;
+	newnode->prev = NULL;
+	*head = newnode;
+	return (*head);
+}
