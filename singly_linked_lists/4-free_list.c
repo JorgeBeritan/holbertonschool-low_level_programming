ff8d7a101fa690020cdb23502db717104d15b85e
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-27 07:15:20 -0400

    FREE memory allocate

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
