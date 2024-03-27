90818599af41042eda9d4a09ade22414be0bd6a2
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-27 14:40:40 -0300

    GEt a struct

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+		{
+			break;
+		}
+		i++;
+		head = head->next;
+	}
+	return (head);
+}
