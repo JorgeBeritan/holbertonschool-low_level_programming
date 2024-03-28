d3e301f9e9d89e6020cb79abfc77b76b93977d4f
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-28 09:14:25 -0300

    Fix Betty Warnings

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,18 +1,15 @@
 #include "lists.h"
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t
+*insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *prev, *newnode;
 
 	if (!idx)
-	{
-		return (add_dnodeint(h,n));
-	}
+		return (add_dnodeint(h, n));
 
 	if (!h)
-	{
 		return (NULL);
-	}
 
 	prev = *h;
 
@@ -20,28 +17,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	while (--idx && prev->next)
-	{
 		prev = prev->next;
-	}
 
 	if (idx)
-	{
 		return (NULL);
-	}
 
 	newnode = malloc(sizeof(*newnode));
 	if (!newnode)
-	{
 		return (NULL);
-	}
 	newnode->n = n;
 	newnode->prev = prev;
 	newnode->next = prev->next;
 
 	if (prev->next)
-	{
 		prev->next->prev = newnode;
-	}
 	prev->next = newnode;
 
 	return (newnode);
