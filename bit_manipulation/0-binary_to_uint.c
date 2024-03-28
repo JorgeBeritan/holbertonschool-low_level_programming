8e56e5d2cd24e1ff99b8c9852c8515df5278c213
JorgeBeritan <inaikmcontacto@gmail.com>
2024-03-28 12:02:14 -0300

    REsolve kernel problems

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-unsigned int binary_to_uint(const char *b)
+unsigned int
+binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int result = 0;
@@ -10,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if(b[i] < '0' || b[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		result = result * 2 + (b[i] - '0');
 	}
